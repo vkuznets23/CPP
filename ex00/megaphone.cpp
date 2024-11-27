d44bc2c9f9c624ebf507257c032e6226a95192ed
Viktoriia Kuznetsova <victoria.cuzneczowa23@gmail.com>
2024-11-27 10:50:00 +0200

    small fix

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,17 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vkuznets <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/27 10:47:18 by vkuznets          #+#    #+#             */
+/*   Updated: 2024/11/27 10:49:42 by vkuznets         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 
 int	main(int ac, char * av[])
 {
-	if (ac == 1)
+	if (ac < 2)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (int i = 1; i < ac; i++)
 	{
 		for (int j = 0; av[i][j] != '\0'; j++)
-		{	
-			if (std::islower(av[i][j]))
-				av[i][j] = std::toupper(av[i][j]);
-		}	
+			av[i][j] = std::toupper(av[i][j]);
 	}
+
 	// Print the transformed arguments
 	for (int i = 1; i < ac; i++)
 		std::cout << av[i] << " ";
