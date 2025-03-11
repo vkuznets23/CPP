7c913b468fc30fea89bc2787accd1018bcb8f871
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-03-11 14:39:13 +0200

    started last task

---
+++ CPP09/ex02/PmergeMe.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: viktoria <viktoria@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/11 14:29:38 by viktoria          #+#    #+#             */
+/*   Updated: 2025/03/11 14:38:49 by viktoria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #pragma once
+// #include <vector>
+// #include <deque>
+
+// class PmergeMe
+// {
+// private:
+//     std::vector<int> _vec;
+//     std::deque<int> _deq;
+
+// public:
+//     PmergeMe();
+//     PmergeMe(const PmergeMe &o);
+//     PmergeMe &operator=(const PmergeMe &o);
+//     ~PmergeMe();
+// }
